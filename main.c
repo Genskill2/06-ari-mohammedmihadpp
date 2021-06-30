425442545ec0cc970cd8d1aa57a2afd7e1b4ebe5
mohammedmihadpp <84678954+mohammedmihadpp@users.noreply.github.com>
2021-06-30 18:29:29 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,35 @@
-#include <stdio.h>
-#include <assert.h>
-#include <string.h>
-#include <cs50.h>
+#include<stdio.h>
+#include<string.h>
+#include<cs50.h>
+#include<ctype.h>
+#include<math.h>
 
-string ari(string);
-
-
-void test1() {
-  string s1="The town was large enough to support three inns, and all their common rooms were rife with rumors. Arianne sent her men into each of them, to hear what they might hear. In the Broken Shield, Daemon Sand was told that the great septry on the Holf of Men had been burned and looted by raiders from the sea, and a hundred young novices from the motherhouse on Maiden Isle carried off into slavery.";
-  assert(strcmp(ari(s1), "Eleventh Grade") == 0);
-}
-
-void test2() { 
- string s2="A mouse took a stroll through the deep dark wood. A fox saw the mouse, and the mouse looked good. Where are you going to, little brown mouse? Come and have lunch in my underground house.";
-  assert(strcmp(ari(s2), "Third Grade") == 0);
-}
-
-void test3() {
-  string s3="Exhaled respiratory particles can build-up within enclosed spaces with inadequate ventilation. The risk of COVID-19 infection increases especially in spaces where people engage in physical exertion or raise their voice (e.g., exercising, shouting, singing) as this increases exhalation of respiratory droplets. Prolonged exposure to these conditions, typically more than 15 minutes, leads to higher risk of infection.";
-  assert(strcmp(ari(s3), "Professor") == 0);
- }
-
-int main(void) {
-  test1();
-  test2();
-  test3();
-}
-
-
+string ari(string s){
+	int len,num_word=0,num_char=0,num_sent=0;
+	string grade;
+	len = strlen(s);
+	//Find the number of characters, words and sentences.
+	for(int i=0;i<len;i++){
+	  if(isalnum(s[i]))
+	    num_char++;
+	  if(s[i]==' ')
+	    num_word++;
+	  if(s[i]=='!'||s[i]=='?'||s[i]=='.')
+	    num_sent++;
+	    }
+	//ARI value formula
+	float ari_value = 4.71*num_char/num_word+0.5*num_word/num_sent-21.43;
+	//Round Resultant score to next highest integer
+	int r=round(ari_value);
+	if(r<ari_value)
+	  r++;
+	//Return the matching string
+	string grade_level[] = {"KinderKindergarten","First/Second Grade","Third Grade","Fourth Grade","Fifth Grade","Sixth Grade","Seventh Grade","Eighth Grade","Ninth Grade","Tenth Grade","Eleventh Grade","Twelfth grade","College student","Professor"} ;
+	
+	for(int i =1;i<=14;i++){
+	 if(r==i)
+	   grade = grade_level[i-1];
+		
+	}
+	return grade;
+	}
